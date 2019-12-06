272e996d795b7a0d5de85f8098ef1d6770a0cc2a
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-06 14:54:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,19 +10,19 @@ def directors_totals(nds)
   pp nds[0]
   
   
-  # counter = 0 
-  # total = 0 
-  # while counter < nds.length do 
-  #   pp nds[counter]
-  #   inner_counter = 0 
-  #   while inner_counter < nds[counter][:movies].length do 
-  #     total += nds[counter][:movies][inner_counter][:worldwide_gross]
-  #     inner_counter +=1 
-  #   end
-  #   puts total 
-  #   total = 0 
-  #   counter +=1
-  # end
+  counter = 0 
+  total = 0 
+  while counter < nds.length do 
+    pp nds[counter]
+    inner_counter = 0 
+    while inner_counter < nds[counter][:movies].length do 
+      total += nds[counter][:movies][inner_counter][:worldwide_gross]
+      inner_counter +=1 
+    end
+    puts total 
+    total = 0 
+    counter +=1
+  end
     
   # counter = 0 
   # total = 0 
